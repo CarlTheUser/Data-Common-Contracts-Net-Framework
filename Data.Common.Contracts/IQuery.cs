dbba92e6c0e2d161aa8aff3083a08b84504a35e9
carlandres18@gmail.com <carlandres18@gmail.com>
2022-04-09 12:24:27 +0800

    Allow multiple arguments in IQuery without creating argument classes

--- Data.Common.Contracts/IQuery.cs
+++ Data.Common.Contracts/IQuery.cs
@@ -8,9 +8,44 @@ namespace Data.Common.Contracts
         TReturn Execute();
     }
 
-    public interface IQuery<TReturn, TParameter>
+    public interface IQuery<TReturn, in TParameter>
     {
         TReturn Execute(TParameter parameter);
+    }
+
+    public interface IQuery<TReturn, in TParameter1, in TParameter2>
+    {
+        TReturn Execute(TParameter1 parameter1, TParameter2 parameter2);
+    }
+
+    public interface IQuery<TReturn, in TParameter1, in TParameter2, in TParameter3>
+    {
+        TReturn Execute(TParameter1 parameter1, TParameter2 parameter2, TParameter3 parameter3);
+    }
+
+    public interface IQuery<TReturn, in TParameter1, in TParameter2, in TParameter3, in TParameter4>
+    {
+        TReturn Execute(TParameter1 parameter1, TParameter2 parameter2, TParameter3 parameter3, TParameter4 parameter4);
+    }
+
+    public interface IQuery<TReturn, in TParameter1, in TParameter2, in TParameter3, in TParameter4, in TParameter5>
+    {
+        TReturn Execute(TParameter1 parameter1, TParameter2 parameter2, TParameter3 parameter3, TParameter4 parameter4, TParameter5 parameter5);
+    }
+
+    public interface IQuery<TReturn, in TParameter1, in TParameter2, in TParameter3, in TParameter4, in TParameter5, in TParameter6>
+    {
+        TReturn Execute(TParameter1 parameter1, TParameter2 parameter2, TParameter3 parameter3, TParameter4 parameter4, TParameter5 parameter5, TParameter6 parameter6);
+    }
+
+    public interface IQuery<TReturn, in TParameter1, in TParameter2, in TParameter3, in TParameter4, in TParameter5, in TParameter6, in TParameter7>
+    {
+        TReturn Execute(TParameter1 parameter1, TParameter2 parameter2, TParameter3 parameter3, TParameter4 parameter4, TParameter5 parameter5, TParameter6 parameter6, TParameter7 parameter7);
+    }
+
+    public interface IQuery<TReturn, in TParameter1, in TParameter2, in TParameter3, in TParameter4, in TParameter5, in TParameter6, in TParameter7, in TParameter8>
+    {
+        TReturn Execute(TParameter1 parameter1, TParameter2 parameter2, TParameter3 parameter3, TParameter4 parameter4, TParameter5 parameter5, TParameter6 parameter6, TParameter7 parameter7, TParameter8 parameter8);
     }
 
     public interface IAsyncQuery<TReturn>
@@ -21,5 +56,40 @@ namespace Data.Common.Contracts
     public interface IAsyncQuery<TReturn, TParameter>
     {
         Task<TReturn> ExecuteAsync(TParameter parameter, CancellationToken token);
+    }
+
+    public interface IAsyncQuery<TReturn, in TParameter1, in TParameter2>
+    {
+        Task<TReturn> ExecuteAsync(TParameter1 parameter1, TParameter2 parameter2, CancellationToken token);
+    }
+
+    public interface IAsyncQuery<TReturn, in TParameter1, in TParameter2, in TParameter3>
+    {
+        Task<TReturn> ExecuteAsync(TParameter1 parameter1, TParameter2 parameter2, TParameter3 parameter3, CancellationToken token);
+    }
+
+    public interface IAsyncQuery<TReturn, in TParameter1, in TParameter2, in TParameter3, in TParameter4>
+    {
+        Task<TReturn> ExecuteAsync(TParameter1 parameter1, TParameter2 parameter2, TParameter3 parameter3, TParameter4 parameter4, CancellationToken token);
+    }
+
+    public interface IAsyncQuery<TReturn, in TParameter1, in TParameter2, in TParameter3, in TParameter4, in TParameter5>
+    {
+        Task<TReturn> ExecuteAsync(TParameter1 parameter1, TParameter2 parameter2, TParameter3 parameter3, TParameter4 parameter4, TParameter5 parameter5, CancellationToken token);
+    }
+
+    public interface IAsyncQuery<TReturn, in TParameter1, in TParameter2, in TParameter3, in TParameter4, in TParameter5, in TParameter6>
+    {
+        Task<TReturn> ExecuteAsync(TParameter1 parameter1, TParameter2 parameter2, TParameter3 parameter3, TParameter4 parameter4, TParameter5 parameter5, TParameter6 parameter6, CancellationToken token);
+    }
+
+    public interface IAsyncQuery<TReturn, in TParameter1, in TParameter2, in TParameter3, in TParameter4, in TParameter5, in TParameter6, in TParameter7>
+    {
+        Task<TReturn> ExecuteAsync(TParameter1 parameter1, TParameter2 parameter2, TParameter3 parameter3, TParameter4 parameter4, TParameter5 parameter5, TParameter6 parameter6, TParameter7 parameter7, CancellationToken token);
+    }
+
+    public interface IAsyncQuery<TReturn, in TParameter1, in TParameter2, in TParameter3, in TParameter4, in TParameter5, in TParameter6, in TParameter7, in TParameter8>
+    {
+        Task<TReturn> ExecuteAsync(TParameter1 parameter1, TParameter2 parameter2, TParameter3 parameter3, TParameter4 parameter4, TParameter5 parameter5, TParameter6 parameter6, TParameter7 parameter7, TParameter8 parameter8, CancellationToken token);
     }
 }
